55a4156884f69424539856efe6e5371053e0556b
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-03-11 12:02:56 +0100

    aggiornamento fx controllers

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -10,6 +10,8 @@ function index(req, res) {
     const sql = 'SELECT * FROM movies'
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
+
+        // se funziona tutto
         res.json(results);
     });
 }
@@ -21,11 +23,31 @@ function show(req, res) {
     // query richiamo singolo movie tramite ID
     const sql = 'SELECT * FROM movies WHERE id = ?';
 
+    // prepariamo la query di richiesta
+    const reviewSql = "SELECT * FROM reviews WHERE movie_id = ?";
+
     //chiamata tramite mysql a movies db
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         if (results.length === 0) return res.status(404).json({ error: 'Movie not found' });
-        res.json(results[0]);
+        // se funziona tutto
+        // res.json(results[0]);
+        const movie = movieResult[0];
+
+        connection.query(reviewSql, [id], (err, reviewResult) => {
+            // se la query non va a buon fine
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+
+            // aggiorniamo l'oggetto movie con le review ritornate
+            movie.reviews = reviewResult;
+
+            // aggiungiamo il valore path img da middleware
+            movie.image = req.imagePath + movie.image;
+
+            // ritorniamo l'oggetto completo
+            res.json(movie);
+        });
+
     });
 }
 
