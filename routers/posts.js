d4c59789d7e90c0997193f1fc8781259e6afb63e
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-03-11 09:30:03 +0100

    creazione rotte index e show nei controllers

--- routers/posts.js
+++ routers/posts.js
@@ -3,10 +3,13 @@ const express = require('express')
 const router = express.Router();
 
 //index//
-app.get('/posts', function (req, res) {
+router.get('/posts', function (req, res) {
     res.send(`Lista dei post`);
 });
 //show//
-app.get('/posts/:id', function (req, res) {
+router.get('/posts/:id', function (req, res) {
     res.send(`Dettagli del post` + req.params.id);
 });
+
+//Esporto
+module.exports = router;
