60250552d606dbc82ba29d67d5bb306aa8046e38
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-03-11 10:38:47 +0100

    aggiorno gitignore

--- data/db.js
+++ data/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const connection = msql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'HorasDundragon93!',
-    database: 'movies_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 connection.connect((err) => {
     if (err) throw err;
